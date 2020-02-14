9d741fe612232883fccd2af4245e4923c7efecc8
sannidhsinghal <49746384+sannidhsinghal@users.noreply.github.com>
2020-02-14 18:02:39 +0530

    Basic user functions

---
+++ src/main/groovy/com/sannidh/practice/domain/dto/User.groovy
@@ -0,0 +1,11 @@
+package com.sannidh.practice.domain.dto
+
+class User {
+    Long id
+
+    String username
+
+    String emailId
+
+    String password
+}
---
+++ src/main/groovy/com/sannidh/practice/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.sannidh.practice.service;
+
+
+import com.sannidh.practice.domain.dto.User;
+import com.sannidh.practice.entity.UserEntity;
+import com.sannidh.practice.exception.UnProccesableException;
+import com.sannidh.practice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+   @Autowired
+   private UserRepository userRepository;
+
+   @Autowired
+   private ModelMapper modelMapper;
+
+
+
+   @Override
+   public List<User> getAllUsers(){
+     return userRepository.findAll().stream().map(value ->modelMapper.map(value, User.class)).collect(Collectors.toList());
+   }
+
+   @Override
+   public User createUser(User user){
+       UserEntity userEntity = modelMapper.map(user,UserEntity.class);
+       userRepository.save(userEntity);
+       return modelMapper.map(userEntity, User.class);
+   }
+
+
+    @Override
+   public User login(String name , String password){
+      Optional<UserEntity> optionalEntity = userRepository.findByUsername(name);
+      if(!optionalEntity.isPresent()){
+          throw new UnProccesableException("Username or password is incorrect");
+      }
+      UserEntity userEntity = optionalEntity.get();
+      return modelMapper.map(userEntity,User.class);
+   }
+}
